41f0e40283f17a589a0d384c63546b59021f57b8
blokkendoos <jvo@chaosgeordend.nl>
2020-01-28 12:30:42 +0100

    Code formatted

--- src/visolate/misc/Util.java
+++ src/visolate/misc/Util.java
@@ -28,17 +28,15 @@ public class Util {
 
   public static final double MIN_DY = 1e-6;
 
-  public static float distance(float x0, float y0,
-                                float x1, float y1) {
+  public static float distance(float x0, float y0, float x1, float y1) {
     return (float) Math.sqrt(distanceSquared(x0, y0, x1, y1));
   }
 
-  public static float distanceSquared(float x0, float y0,
-                                       float x1, float y1) {
-    double dx = x1-x0;
-    double dy = y1-y0;
+  public static float distanceSquared(float x0, float y0, float x1, float y1) {
+    double dx = x1 - x0;
+    double dy = y1 - y0;
 
-    return (float) (dx*dx + dy*dy);
+    return (float) (dx * dx + dy * dy);
   }
 
   public static double canonicalAngle(double x, double y) {
@@ -46,18 +44,18 @@ public class Util {
     double angle = Math.atan2(y, x);
 
     if (angle < 0.0)
-      angle += Math.PI*2.0;
+      angle += Math.PI * 2.0;
 
     return angle;
   }
 
   public static double canonicalizeAngle(double theta) {
 
-    while (theta > Math.PI*2)
-      theta -= Math.PI*2;
+    while (theta > Math.PI * 2)
+      theta -= Math.PI * 2;
 
     while (theta < 0)
-      theta += Math.PI*2;
+      theta += Math.PI * 2;
 
     return theta;
   }
@@ -73,14 +71,14 @@ public class Util {
     if (closest < 0) {
 
       int next = -closest - 1;
-      
+
       int prev = next - 1;
-      
+
       if (next == angle.length)
         next = 0;
-      
+
       if (prev < 0)
-        prev = angle.length-1;
+        prev = angle.length - 1;
 
       if ((theta - angle[prev]) < (angle[next] - theta))
         closest = prev;
@@ -91,9 +89,8 @@ public class Util {
     return closest;
   }
 
-  public static double getPolyWidth(float[] x, float[] y, double[] angle,
-                                    double theta) {
-    
+  public static double getPolyWidth(float[] x, float[] y, double[] angle, double theta) {
+
     theta = canonicalizeAngle(theta);
 
     double dx = Math.cos(theta);
@@ -101,7 +98,7 @@ public class Util {
 
     int i = findClosest(theta, angle);
 
-    double d1 = dx*x[i] + dy*y[i];
+    double d1 = dx * x[i] + dy * y[i];
 
     theta = canonicalizeAngle(theta + Math.PI);
 
@@ -110,16 +107,16 @@ public class Util {
 
     i = findClosest(theta, angle);
 
-    double d2 = dx*x[i] + dy*y[i];
+    double d2 = dx * x[i] + dy * y[i];
 
-    return Math.abs(d1)+Math.abs(d2);
+    return Math.abs(d1) + Math.abs(d2);
   }
 
   /**
    * is this just asin?
    **/
   public static double getOffsetAngle(double rad, double y) {
-    return Math.atan2(y, Math.sqrt(rad*rad-y*y));
+    return Math.atan2(y, Math.sqrt(rad * rad - y * y));
   }
 
   public static double angleCCW(Vector3d from, Vector3d to) {
@@ -127,32 +124,30 @@ public class Util {
     double angleF = canonicalizeAngle(Math.atan2(from.y, from.x));
     double angleT = canonicalizeAngle(Math.atan2(to.y, to.x));
 
-    return canonicalizeAngle(angleT-angleF);
+    return canonicalizeAngle(angleT - angleF);
   }
 
   public static double vertexOffset(double offset, double angle) {
-    
-    Vector2d p0 = new Vector2d(Math.cos(angle-Math.PI/2),
-                              Math.sin(angle-Math.PI/2));
-    
+
+    Vector2d p0 = new Vector2d(Math.cos(angle - Math.PI / 2), Math.sin(angle - Math.PI / 2));
+
     p0.scale(-offset);
 
-    Vector2d p1 = new Vector2d(p0.x + Math.cos(angle),
-                               p0.y + Math.sin(angle));
-    
-    double dy = p1.y-p0.y;
+    Vector2d p1 = new Vector2d(p0.x + Math.cos(angle), p0.y + Math.sin(angle));
+
+    double dy = p1.y - p0.y;
 
     if (dy < MIN_DY)
       return offset;
 
-    double dx = p1.x-p0.x;
+    double dx = p1.x - p0.x;
 
-    double xIntercept = p0.x - (dx/dy)*p0.y;
+    double xIntercept = p0.x - (dx / dy) * p0.y;
 
     double y = -offset;
-    double x = xIntercept + (dx/dy)*y;
+    double x = xIntercept + (dx / dy) * y;
 
-    return Math.sqrt(x*x + y*y)*sign(offset);
+    return Math.sqrt(x * x + y * y) * sign(offset);
   }
 
   public static double sign(double d) {
@@ -164,6 +159,3 @@ public class Util {
       return 1;
   }
 }
-
-
-
